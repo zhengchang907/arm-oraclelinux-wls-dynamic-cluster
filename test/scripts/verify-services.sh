0ec6a6272b7e307873d00dd509ae5030c490fca4
sanjaymantoor <36834780+sanjaymantoor@users.noreply.github.com>
2020-06-26 18:55:22 +0530

    Adding wls-services verification to CI/CD (#47)

--- test/scripts/verify-services.sh
+++ test/scripts/verify-services.sh
@@ -2,7 +2,7 @@
 function verifyServiceStatus()
 {
   serviceName=$1
-  systemctl status rngd | grep "active (running)"    
+  systemctl status $serviceName | grep "active (running)"    
   if [[ $? != 0 ]]; then
      echo "$serviceName is not in active (running) state"
      exit 1
@@ -23,15 +23,11 @@ function verifyServiceActive()
   fi
 }
 
-# Pass yes/no
-export isAdminServer=$1
-if [[ $isAdminServer == "yes" ]]; then
-  echo "Testing on admin server"
-  servicesList="rngd wls_nodemanager wls_admin"
-else
-  echo "Testing on managed server"
-  servicesList="rngd wls_nodemanager"
-fi
+# Pass the services to be checked based on admin/managed servers
+# For admin server    : rngd wls_admin wls_nodemanager
+# For managed server  : rngd wls_nodemanager
+
+export servicesList=$*
 
 for service in $servicesList
 do
